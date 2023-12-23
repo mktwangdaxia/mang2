fe58b8173bddbcb5d1ac88a41fccf7d89dd34653
mktwangdaxia <2268181310@qq.com>
2023-12-23 12:11:54 +0800

    wotijiaole

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,19 @@
+<script setup lang="ts">
+  import {ref} from 'vue';
+  const count =ref(0)
+  const onClick=()=>{
+    count.value +=1
+  }
+</script>
 <template>
   <div>
-    hi
+    {{count}}
+  </div>
+  <div>
+  <button @click="onClick">+1</button>
   </div>
 </template>
+
+<style lang="" scoped>
+  
+</style>
